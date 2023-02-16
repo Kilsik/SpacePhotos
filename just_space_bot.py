d8cdbd6a81529da8bb2d5ac254b3c0e97bd46cb1
Kilsik <ilsiwow@gmail.com>
2023-02-16 14:43:35 +0300

    Add publication schedule

--- just_space_bot.py
+++ just_space_bot.py
@@ -1,26 +1,56 @@
 import telegram
 import os
 import argparse
+import random
 
 from dotenv import load_dotenv
+from time import sleep
 
 
-def publish(text, photo):
+def schedule():
+    parser = argparse.ArgumentParser(
+        description='Publish docs from folders according to the schedule'
+        )
+    parser.add_argument(
+        'text_folder',
+        help='Folder with text files'
+        )
+    parser.add_argument(
+        'photo_folder',
+        help='Folder with photos'
+        )
+    args = parser.parse_args()
     load_dotenv()
+    list_text = list(os.walk(args.text_folder))[0][2]
+    list_photo = list(os.walk(args.photo_folder))[0][2]
+    photo = 'images/nasa_apod1.jpg'
+    timer = int(os.environ["TIMER"])
+    while True:
+        for photo in list_photo:
+            publish_photo(os.path.join(args.photo_folder,photo))
+            sleep(timer)
+        random.shuffle(list_photo)
+
+
+def publish_text(text):
     bot_token = os.environ["BOT_TOKEN"]
     space_bot = telegram.Bot(token=bot_token)
     chat_id = os.environ["CHANEL_ID"]
-    with open(text, 'r') as text_file:
+    with open(text, 'r', encoding='UTF-8') as text_file:
         text_message = text_file.read()
     space_bot.send_message(chat_id=chat_id, text=text_message)
+
+
+def publish_photo(photo):
+    bot_token = os.environ["BOT_TOKEN"]
+    space_bot = telegram.Bot(token=bot_token)
+    chat_id = os.environ["CHANEL_ID"]
     with open(photo, 'rb') as image_file:
         space_bot.send_document(chat_id=chat_id, document=image_file)
 
 
 def main():
-    #image = 'images/nasa_apod1.jpg'
-    #space_bot.send_document(chat_id=chat_id, document=open(image, 'rb'))
-    publish('texts/Artemis 1_1.txt', 'images/nasa_apod1.jpg')
+    schedule()
 
 if __name__ == '__main__':
     main()
