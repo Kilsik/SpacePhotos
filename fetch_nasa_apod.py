1c8ac87666adda877b8f58b29fdb80e29f0a7a2b
Kilsik <ilsiwow@gmail.com>
2023-02-17 14:38:41 +0300

    Formated code

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -11,19 +11,20 @@ def fetch_nasa_apod():
         description='Downloads NASA photo of day'
         )
     parser.add_argument('folder',
-    type=str,
-    help='Output dir for image'
-    )
+                        type=str,
+                        help='Output dir for image'
+                        )
     parser.add_argument('-count',
-    type=int,
-    default=10,
-    help='Count randomly chosen images (default 10)')
+                        type=int,
+                        default=10,
+                        help='Count randomly chosen images (default 10)'
+                        )
     args = parser.parse_args()
     load_dotenv()
     nasa_key = os.environ["NASA_API_KEY"]
     nasa_params = {"api_key": nasa_key,
-                    "count": args.count
-    }
+                   "count": args.count
+                   }
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     nasa_response = requests.get(nasa_url, params=nasa_params)
     nasa_response.raise_for_status()
@@ -31,7 +32,7 @@ def fetch_nasa_apod():
     for link_number, link in enumerate(link_list):
         try:
             ext = files_ext(link["hdurl"])
-        except:
+        except KeyError:
             continue
         path = f'{args.folder}/nasa_apod{link_number}{ext}'
         get_image(link["hdurl"], path)
@@ -39,6 +40,7 @@ def fetch_nasa_apod():
 
 def main():
     fetch_nasa_apod()
+
 
 if __name__ == '__main__':
     main()
