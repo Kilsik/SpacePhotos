dce84ef208bcc3e59b06fecda58eb39183a15d3b
Kilsik <ilsiwow@gmail.com>
2023-02-17 15:47:05 +0300

    Renaming

--- publisher_bot.py
+++ publisher_bot.py
@@ -22,10 +22,10 @@ def scheduled_publishing():
         default=os.environ["TIMER"],
         help='Timer, sec.')
     args = parser.parse_args()
-    list_photo = list(os.walk(args.photo_folder))[0][2]
+    roster_photo = list(os.walk(args.photo_folder))[0][2]
     timer = args.timer
     while True:
-        for photo in list_photo:
+        for photo in roster_photo:
             publish_photo(os.path.join(args.photo_folder, photo))
             sleep(timer)
         random.shuffle(list_photo)
